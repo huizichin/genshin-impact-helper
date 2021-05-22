63b4676fc67cf500ce67050122b813ac5fccb64f
huizichin <huizichin@gmail.com>
2021-05-22 08:23:38 +0800

    added script to offset the time difference

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import random, time, os, schedule
-os.environ['TZ'] = 'Asia/China'
+import datetime as dt
+#This will offset the server time on heroku it is UTC+0, Change this accordingly.
+utc_timezone = +8
+#Time the script will be ran
+run_time = '06:30'
+
+tt = dt.datetime.strptime(run_time, '%H:%M')
+ha = dt.timedelta(hours = utc_timezone)
+lt = tt + ha
+fot = lt.strftime("%H:%M")
 
 def job():
     randomSleep = random.randint(10,300)
     print("Sleeping for: %ds" % randomSleep)
     time.sleep(randomSleep)
     exec(open('./run.py').read())
+    rs = True
 
-schedule.every().day.at("06:00").do(job)
+schedule.every().day.at(fot).do(job)
 
 print("Script Started")
 
